76f2883a7b8a37fe2b8e8f9d6be010ec46b6881e
Yasir Nawaz <y451rmahar@gmail.com>
2024-04-12 23:34:14 +0500

    :wrench: List Item fix in ContentList.jsx

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -133,7 +133,9 @@ export default function ContentList({
                 key={index}
                 className="list-item opacity-0"
                 onMouseEnter={() => onMouseEnter(index)}
-                ref={(el) => (itemsRef.current[index] = el)}
+                ref={(el) => {
+                  if (el) itemsRef.current[index] = el;
+                }}
               >
                 <Link
                   href={urlPrefixe + "/" + item.uid}
