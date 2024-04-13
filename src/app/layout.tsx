727cc51e824f0aaa0743c6db8cec5b010f4cbb6d
Yasir Nawaz <y451rmahar@gmail.com>
2024-04-13 10:53:56 +0500

    :fire: to stash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,61 +1,43 @@
+import "./globals.css";
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import clsx from "clsx";
-import { createClient, repositoryName } from "@/prismicio";
 import { PrismicPreview } from "@prismicio/next";
+import { createClient, repositoryName } from "@/prismicio";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
-const metadata = {
-  title: "Yasir Nawaz - Portfolio",
-  description: "Welcome to Yasir Nawaz's portfolio website. Explore my projects and learn more about my skills and experience in web development.",
-  keywords: ["Yasir Nawaz", "portfolio", "web development", "JavaScript", "React", "projects", "skills", "experience"],
-  authors: [{ name: "Yasir Nawaz", url: "https://linkedin.com/in/yasirnawaz24" }],
-  referrer: "origin",
-  themeColor: "#0070f3",
-  colorScheme: "light",
-  canonical: new URL("https://yasirnawaz.vercel.app"),
-  openGraph: {
-    type: "website",
-    url: "https://yasirnawaz.vercel.app",
-    title: "Yasir Nawaz - Portfolio",
-    description: "Welcome to Yasir Nawaz's portfolio website. Explore my projects and learn more about my skills and experience in web development.",
-    siteName: "Yasir Nawaz - Portfolio",
-    images: [{ url: "https://drive.google.com/file/d/16dkNyroFXYq2AdEIRp4iHNnXggYckfnz/view?usp=sharing", alt: "Yasir Nawaz - Portfolio" }]
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@stfuyasir",
-    creator: "@stfuyasir"
-  }
-};
 
-export async function generateMetaData(): Promise<Metadata> {
+export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
   const settings = await client.getSingle("settings");
+
   return {
     title: settings.data.meta_title,
     description: settings.data.meta_description,
+    // openGraph: {
+    //   images: [settings.data.og_image?.url || ""],
+    // },
   };
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className="bg-slate-900 text-slate-100">
+    <html lang="en" className="bg-slate-900">
       <body className={clsx(urbanist.className, "relative min-h-screen")}>
         <Header />
         {children}
-        <Footer />
-        <div className="background-gradient absolute inset-0 -z-50 max-h-screen"></div>
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
         <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
+        <Footer />
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
-      <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { useEffect, useRef } from "react";
-
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import { gsap } from "gsap";
+import { Shapes } from "@/slices/Hero/Shapes";
 import Bounded from "@/components/Bounded";
-import { Shapes } from "./Shapes";
+import gsap from "gsap";
 
 /**
  * Props for `Hero`.
@@ -16,56 +15,52 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-  const components = useRef(null);
+  const component = useRef(null);
 
   useEffect(() => {
     let ctx = gsap.context(() => {
-      const tl = gsap.timeline();
-      tl.fromTo(
-        ".name-animation",
-        {
-          x: -100,
-          opacity: 0,
-          rotate: -10,
-        },
-        {
-          x: 0,
-          opacity: 1,
-          rotate: 0,
-          ease: "elastic.out(1,0.3)",
-          duration: 1,
-          transformOrigin: "left top",
-          delay: 0.5,
-          stagger: {
-            each: 0.1,
-            from: "random",
+      // create as many GSAP animations and/or ScrollTriggers here as you want...
+      gsap
+        .timeline()
+        .fromTo(
+          ".name-animation",
+          { x: -100, opacity: 0, rotate: -10 },
+          {
+            x: 0,
+            opacity: 1,
+            rotate: 0,
+
+            ease: "elastic.out(1,0.3)",
+            duration: 1,
+            transformOrigin: "left top",
+            stagger: { each: 0.1, from: "random" },
           },
-        }
-      );
-      tl.fromTo(
-        ".job-title",
-        {
-          opacity: 0,
-          scale: 1.2,
-        },
-        {
-          opacity: 1,
-          y: 0,
-          duration: 1,
-          scale: 1,
-          ease: "elastic.out(1,0.3)",
-        }
-      );
-    }, components);
-    return () => ctx.revert();
+        )
+        .fromTo(
+          ".job-title",
+          {
+            y: 20,
+            opacity: 0,
+            scale: 1.2,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            scale: 1,
+            ease: "elastic.out(1,0.3)",
+          },
+        );
+    }, component);
+    return () => ctx.revert(); // cleanup!
   }, []);
 
   const renderLetters = (name: KeyTextField, key: string) => {
     if (!name) return;
     return name.split("").map((letter, index) => (
       <span
-        key={key + index}
-        className={`name-animation name-animation-${key} inline-block opacity-0`}
+        key={index}
+        className={`name-animation name-animation-${key}-index inline-block opacity-0 `}
       >
         {letter}
       </span>
@@ -76,25 +71,25 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      ref={components}
+      ref={component}
     >
-      <div className="grid min-h-[70vh] grid-cols-2 md:grid-cols-2 items-center">
+      <div className="grid min-h-[70vh] grid-cols-1 items-center md:grid-cols-2">
         <Shapes />
-        <div className="col-start-1 md:row-start-1">
+        <div className="col-start-1 md:row-start-1 " data-speed=".2">
           <h1
             className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
             aria-label={
               slice.primary.first_name + " " + slice.primary.last_name
             }
           >
-            <span className="block text-slate-300">
+            <span className="block text-slate-300 ">
               {renderLetters(slice.primary.first_name, "first")}
             </span>
-            <span className="-mt-[.2rem] block text-slate-500">
+            <span className="-mt-[.2em] block text-slate-500  ">
               {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
-          <span className="job-title opacity-0 block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2rem] text-transparent md:text-4xl">
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
             {slice.primary.tag_line}
           </span>
         </div>
