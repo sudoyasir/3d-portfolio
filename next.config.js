da8367acd1046adfc4d7596fb77ee9e5e3fa83b2
Yasir Nawaz <y451rmahar@gmail.com>
2024-04-14 19:35:21 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,9 @@
  */
 const nextConfig = {
   output: "export",
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
