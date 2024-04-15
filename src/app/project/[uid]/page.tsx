58d6b829a5753feaf9e563545bcf4c481f897e38
Yasir Nawaz <y451rmahar@gmail.com>
2024-04-15 17:34:04 +0500

    :package: Added Meta manifest image

--- src/app/project/[uid]/page.tsx
+++ src/app/project/[uid]/page.tsx
@@ -28,6 +28,7 @@ export async function generateMetadata({
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
+    manifest: page.data.meta_image.url,
   };
 }
 
