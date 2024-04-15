d0169efb8c18f557fe0e53c19ef1719b6a6cc308
Yasir Nawaz <y451rmahar@gmail.com>
2024-04-15 17:26:38 +0500

    :lipstick: Different colors for "#" in content list

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -1,5 +1,4 @@
 import { SliceZone } from "@prismicio/react";
-
 import { components } from "@/slices";
 import Bounded from "./Bounded";
 import Heading from "./Heading";
@@ -27,13 +26,18 @@ export default function ContentBody({
   }
   const formattedDate = formatDate(page.data.date);
 
+  // Define an array of colors for "#" symbols
+  const hashColors = ["text-blue-400", "text-red-400", "text-green-400", "text-yellow-400"];
+
   return (
     <Bounded as="article">
       <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
         <Heading as="h1">{page.data.title}</Heading>
-        <div className="flex gap-4 text-yellow-400 text-xl font-bold">
-          {page.tags.map((tag) => (
-            <span key={tag}>{tag}</span>
+        <div className="flex gap-4 text-xl">
+          {page.tags.map((tag, index) => (
+            <span key={tag}>
+              <span className={hashColors[index % hashColors.length]}>#</span><span className="text-white">{tag}</span>
+            </span>
           ))}
         </div>
         <p className="mt-8 border-b border-b-slate-600 text-xl font-medium text-slate-300">
